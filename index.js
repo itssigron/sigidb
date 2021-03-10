393ffbb6406e6a4d1290a4f31735f2c7c7d9b880
itssigron <arel1234123@gmail.com>
2021-03-11 00:07:41 +0200

    Made full customization over id/table/data

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-module.exports = function database(name, options) {
+module.exports = function database(name, databaseOptions) {
 const Database = require("better-sqlite3");
 const util = require("./util/util");
 const fs = require("fs");
-const db = new Database(name, options);
+const db = new Database(name, databaseOptions);
+if(util.typeof(databaseOptions) !== "Object") databaseOptions = {};
+if(!databaseOptions.id) databaseOptions.id = "id";
+if(typeof databaseOptions.id !== "string") throw new TypeError("Expected database.options.id to be a string.") 
 
+if(!databaseOptions.value) databaseOptions.value = "value";
+if(typeof databaseOptions.value !== "string") throw new TypeError("Expected database.options.value to be a string.") 
+
+if(!databaseOptions.table) databaseOptions.table = "main";
+if(typeof databaseOptions.table !== "string") throw new TypeError("Expected database.options.table to be a string.") 
 
 let exportsObject = (db, opts) => {
-    db.prepare(`CREATE TABLE IF NOT EXISTS ${opts ? opts.table || "main" : "main"} (id TEXT, value TEXT)`).run();
+    db.prepare(`CREATE TABLE IF NOT EXISTS ${opts ? opts.table || databaseOptions.table : databaseOptions.table} (id TEXT, value TEXT)`).run();
 
     function runMethod(name, ...params) {
         const file = require(`./lib/${name}.js`);
         params = [db].concat(params);
         let options = params[params.length - 1] || params[params.length - 2];
         if (util.typeof(options) !== "Object") options = {};
-        if (!options.table) options.table = "main";
-        db.prepare(`CREATE TABLE IF NOT EXISTS ${options.table} (id TEXT, value TEXT)`).run();
+        if (!options.table) options.table = databaseOptions.table;
+        options.id = databaseOptions.id;
+        options.value = databaseOptions.value;
+        
+        db.prepare(`CREATE TABLE IF NOT EXISTS ${options.table} (${databaseOptions.id} TEXT, ${databaseOptions.value} TEXT)`).run();
         return file(...params.slice(0, params.length - 2).concat(options));
     };
     let obj = {
@@ -23,42 +34,42 @@ let exportsObject = (db, opts) => {
         },
         set: function (key, value, options = {}) {
             if (key === undefined) throw new TypeError("A key must be provided.");
-            if (!value === undefined) throw new TypeError("A value must be provided.");
+            if (value === undefined) throw new TypeError("A value must be provided.");
 
             return runMethod("set", key, value, options, opts)
         },
         get: function (key, options = {}) {
-            if (!key) throw new TypeError("A key must be provided.");
+            if (key === undefined) throw new TypeError("A key must be provided.");
 
             return runMethod("get", key, options, opts)
         },
         fetch: function (key, options = {}) {
-            if (!key) throw new TypeError("A key must be provided.");
+            if (key === undefined) throw new TypeError("A key must be provided.");
 
             return runMethod("get", key, options, opts)
         },
         has: function (key, options = {}) {
-            if (!key) throw new TypeError("A key must be provided.");
+            if (key === undefined) throw new TypeError("A key must be provided.");
 
             return runMethod("has", key, options, opts)
         },
         add: function (key, value, options = {}) {
             if (key === undefined) throw new TypeError("A key must be provided.");
-            if (!value === undefined) throw new TypeError("A value must be provided.");
+            if (value === undefined) throw new TypeError("A value must be provided.");
             if (typeof value !== "number") throw new TypeError("Value's type must be a number.");
 
             return runMethod("add", key, value, options, opts)
         },
         subtract: function (key, value, options = {}) {
             if (key === undefined) throw new TypeError("A key must be provided.");
-            if (!value === undefined) throw new TypeError("A value must be provided.");
+            if (value === undefined) throw new TypeError("A value must be provided.");
             if (typeof value !== "number") throw new TypeError("Value's type must be a number.");
 
             return runMethod("subtract", key, value, options, opts)
         },
         push: function (key, value, options = {}) {
             if (key === undefined) throw new TypeError("A key must be provided.");
-            if (!value === undefined) throw new TypeError("A value must be provided.");
+            if (value === undefined) throw new TypeError("A value must be provided.");
 
             return runMethod("push", key, value, options, opts)
         },
@@ -71,7 +82,7 @@ let exportsObject = (db, opts) => {
             return runMethod("deleteAll", options, opts)
         },
         type: function (key, options = {}) {
-            if (!key) throw new TypeError("A key must be provided.");
+            if (key === undefined) throw new TypeError("A key must be provided.");
 
             return runMethod("type", key, options, opts)
         },
@@ -86,15 +97,15 @@ let exportsObject = (db, opts) => {
         },
         attach: function (name, path, options = {}) {
             throw new Error("This method isnt fully completed and have some issues.");
-            if (!name) throw new TypeError(`Name parameter must be provided.`);
-            if (!path) throw new TypeError(`Path parameter must be provided.`);
+            if (name === undefined) throw new TypeError(`Name parameter must be provided.`);
+            if (path === undefined) throw new TypeError(`Path parameter must be provided.`);
 
             return runMethod("attach", name, path, options, opts);
         },
         detach: function (name, options = {}) {
             throw new Error("This method isnt fully completed and have some issues.");
 
-            if (!name) throw new TypeError(`Name parameter must be provided.`);
+            if (name === undefined) throw new TypeError(`Name parameter must be provided.`);
 
             return runMethod("detach", name, options, opts);
         },
